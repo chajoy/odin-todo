9bd8e870780d86a8f5b2e395913beae763ad9a71
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-30 17:23:46 +0000

    Add project function and task class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import "./styles.css";
+
+const Project = (title) => {
+    const todos = [];
+
+    const getTitle = () => {
+        return title;
+    }
+
+    const getTodo = () => {
+        return todos;
+    }
+
+    const addTask = (task) => {
+        todos.push(task);
+    }
+
+    return {
+        getTitle,
+        addTask,
+        getTodo,
+    }
+};
+
+class Task {
+    constructor(title, desc, dueDate, prio) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.prio = prio;
+    }
+
+
+}
+
+const projects = [];
+
+projects.push(Project(`default`));
+
+projects[0].addTask(new Task(`work`, `do work`, `now`, `urgent`));
+
+console.log(projects[0].getTodo())
