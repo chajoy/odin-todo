32442f9668a1af484aed9366a47f80129c09b85b
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-01-07 18:14:42 +0000

    Set sidebar position to fixed

--- src/pages.js
+++ src/pages.js
@@ -43,54 +43,58 @@ export const projects = (projects_array) => {
         container.appendChild(content.header.container);
 
         //generate projects list and append to main container
-        for (let x = 0; x < projects_array.length; x++) {
-            const project = {
-                container: document.createElement(`div`),
-                title: {
+        if (!projects_array) {
+            console.warn(`Projects Array is Null`);
+        } else {
+            for (let x = 0; x < projects_array.length; x++) {
+                const project = {
                     container: document.createElement(`div`),
-                    text: document.createElement(`h2`),
-                    dropdownIcon: document.createElement(`img`),
-                },
-            }
-
-            project.container.classList.add(`project`);
-            project.title.container.classList.add(`p_title`);
-            project.title.text.classList.add(`f_orange`);
-            project.title.text.textContent = projects_array[x].title;
-            project.title.dropdownIcon.src = dropdownIcon;
-            project.title.dropdownIcon.classList.add(`svg-arrow`);
-
-            project.title.container.appendChild(project.title.text);
-            project.title.container.appendChild(project.title.dropdownIcon);
-            project.container.appendChild(project.title.container);
-
-            for (let y = 0; y < projects_array[x].GetTasks().length; y++) {
-                let taskList = projects_array[x].GetTasks();
-                project.task = {
-                    container: document.createElement(`div`),
-                    checkbox: document.createElement(`input`),
-                    description: {
+                    title: {
                         container: document.createElement(`div`),
-                        title: document.createElement(`h3`),
-                        text: document.createElement(`p`),
-                    }
+                        text: document.createElement(`h2`),
+                        dropdownIcon: document.createElement(`img`),
+                    },
                 }
 
-                project.task.container.classList.add(`p_task`);
-                project.task.checkbox.setAttribute(`type`, `checkbox`);
-                project.task.description.title.textContent = taskList[y].title;
-                project.task.description.text.textContent = taskList[y].desc;
+                project.container.classList.add(`project`);
+                project.title.container.classList.add(`p_title`);
+                project.title.text.classList.add(`f_orange`);
+                project.title.text.textContent = projects_array[x].title;
+                project.title.dropdownIcon.src = dropdownIcon;
+                project.title.dropdownIcon.classList.add(`svg-arrow`);
 
-                project.task.description.container.appendChild(project.task.description.title);
-                project.task.description.container.appendChild(project.task.description.text);
+                project.title.container.appendChild(project.title.text);
+                project.title.container.appendChild(project.title.dropdownIcon);
+                project.container.appendChild(project.title.container);
 
-                project.task.container.appendChild(project.task.checkbox);
-                project.task.container.appendChild(project.task.description.container);
-                project.container.appendChild(project.task.container);
+                for (let y = 0; y < projects_array[x].GetTasks().length; y++) {
+                    let taskList = projects_array[x].GetTasks();
+                    project.task = {
+                        container: document.createElement(`div`),
+                        checkbox: document.createElement(`input`),
+                        description: {
+                            container: document.createElement(`div`),
+                            title: document.createElement(`h3`),
+                            text: document.createElement(`p`),
+                        }
+                    }
+
+                    project.task.container.classList.add(`p_task`);
+                    project.task.checkbox.setAttribute(`type`, `checkbox`);
+                    project.task.description.title.textContent = taskList[y].title;
+                    project.task.description.text.textContent = taskList[y].desc;
+
+                    project.task.description.container.appendChild(project.task.description.title);
+                    project.task.description.container.appendChild(project.task.description.text);
+
+                    project.task.container.appendChild(project.task.checkbox);
+                    project.task.container.appendChild(project.task.description.container);
+                    project.container.appendChild(project.task.container);
+                }
+
+                container.appendChild(project.container);
+
             }
-
-            container.appendChild(project.container);
-
         }
     }
 };
